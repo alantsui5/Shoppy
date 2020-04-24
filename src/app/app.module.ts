eb184340fb6c13ab3f58cbbbd6f5092b7671d06c
alantsui5 <1155110323@link.cuhk.edu.hk>
2020-04-24 17:49:57 +0800

    Fix manifest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -75,7 +75,7 @@ registerLocaleData(en);
     AngularFireAuthModule,
     AngularFireStorageModule,
     AngularFireAuthGuardModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production, registrationStrategy: 'registerImmediately'  }),
     NgZorroAntdModule,
     FormsModule,
     HttpClientModule,
